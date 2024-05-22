f06b843b94e1798062b23d11d24f88bea31c6a52
Braden Kartchner <62575814+BradenKartchner@users.noreply.github.com>
2024-05-22 17:02:32 -0600

    added TODO

--- issue-tracker/app/page.tsx
+++ issue-tracker/app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
     return (
         <main>
-            <div>Hello World!</div>
+            <div className="py-2">
+                Navigate to the issues page for more functionality
+            </div>
+            <div className="py-2">
+                TODO: display all the issues from the database on a page
+            </div>
         </main>
     );
 }
