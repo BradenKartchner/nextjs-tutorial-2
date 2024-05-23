5523e29bc51e0a2ccc60071a4795ddc8c04de5c8
Braden Kartchner <62575814+BradenKartchner@users.noreply.github.com>
2024-05-22 18:00:07 -0600

    client side validation of issue form

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,23 +1,37 @@
 "use client";
 import React from "react";
 import { useState } from "react";
-import { TextField, Button, Callout } from "@radix-ui/themes";
+import { TextField, Button, Callout, Text } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { RxInfoCircled } from "react-icons/rx";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-// interface for shape of the form
+// interface for shape of the form. we can remove due to importing our schema and using z.infer
+/* 
 interface IssueForm {
     title: string;
     description: string;
 }
+*/
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
     const router = useRouter();
-    const { register, control, handleSubmit } = useForm<IssueForm>();
+    const {
+        register,
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema),
+    });
     console.log(register("title"));
     const [error, setError] = useState("");
 
@@ -38,6 +52,7 @@ const NewIssuePage = () => {
                         await axios.post("/api/issues", data);
                         router.push("/issues");
                     } catch (error) {
+                        console.log(error);
                         setError("An unexpected error occurred.");
                     }
                 })}
@@ -46,6 +61,11 @@ const NewIssuePage = () => {
                     placeholder="Title"
                     {...register("title")}
                 ></TextField.Root>
+                {errors.title && (
+                    <Text color="red" as="p">
+                        {errors.title.message}
+                    </Text>
+                )}
                 <Controller
                     name="description"
                     control={control}
@@ -53,6 +73,11 @@ const NewIssuePage = () => {
                         <SimpleMDE placeholder="Description" {...field} />
                     )}
                 />
+                {errors.description && (
+                    <Text color="red" as="p">
+                        {errors.description.message}
+                    </Text>
+                )}
                 <Button>Submit New Issue</Button>
             </form>
         </div>
---
+++ issue-tracker/app/validationSchemas.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+// define zod shape for body of our request
+// we only need "title" and "description", if you look at
+// our Prisma schema all the other fields have default values
+export const createIssueSchema = z.object({
+    title: z.string().min(1, "Title is required.").max(255),
+    description: z.string().min(1, "Description is required."),
+});
