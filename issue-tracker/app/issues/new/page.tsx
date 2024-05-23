649f19192669716b88f97c24e0c3ca72eb20a995
Braden Kartchner <62575814+BradenKartchner@users.noreply.github.com>
2024-05-22 21:19:18 -0600

    added spinner and state protection for submitting issue

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -12,6 +12,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "../../components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 // interface for shape of the form. we can remove due to importing our schema and using z.infer
 /* 
@@ -34,7 +35,9 @@ const NewIssuePage = () => {
         resolver: zodResolver(createIssueSchema),
     });
     console.log(register("title"));
+
     const [error, setError] = useState("");
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
     if (errors) {
         console.log(errors);
@@ -54,9 +57,11 @@ const NewIssuePage = () => {
                 className="space-y-3"
                 onSubmit={handleSubmit(async (data) => {
                     try {
+                        setIsSubmitting(true);
                         await axios.post("/api/issues", data);
                         router.push("/issues");
                     } catch (error) {
+                        setIsSubmitting(false);
                         console.log(error);
                         setError("An unexpected error occurred.");
                     }
@@ -75,7 +80,9 @@ const NewIssuePage = () => {
                     )}
                 />
                 {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
-                <Button>Submit New Issue</Button>
+                <Button disabled={isSubmitting}>
+                    Submit New Issue {isSubmitting && <Spinner />}
+                </Button>
             </form>
         </div>
     );
