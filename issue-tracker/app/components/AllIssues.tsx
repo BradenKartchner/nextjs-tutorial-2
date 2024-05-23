639373cd5fc4f3ed02bfccac8a3eb7d0990ac953
Braden Kartchner <62575814+BradenKartchner@users.noreply.github.com>
2024-05-22 22:01:47 -0600

    prep to pull issues from database

---
+++ issue-tracker/app/components/AllIssues.tsx
@@ -0,0 +1,8 @@
+// Get all issues from Database and display them in a readable format
+import React from "react";
+
+const AllIssues = () => {
+    return <div>AllIssues</div>;
+};
+
+export default AllIssues;
